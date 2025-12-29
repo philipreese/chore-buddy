79ecf7c6bf961f26e7dbd96ba16784c71d214293
Philip Reese <philipreese@mail.com>
2025-12-29 17:43:05 -0500

    add undo of chore completion via snackbar

--- ChoreBuddy/MauiProgram.cs
+++ ChoreBuddy/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ChoreBuddy;
 
@@ -7,14 +8,24 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                fonts.AddFont("fa-solid-900.ttf", "FontAwesome");
-            });
+        builder.UseMauiApp<App>();
+
+#if ANDROID || WINDOWS || TIZEN
+        builder.UseMauiCommunityToolkit();
+#elif IOS || MACCATALYST
+        if (OperatingSystem.IsIOSVersionAtLeast(15) ||
+            OperatingSystem.IsMacCatalystVersionAtLeast(15))
+        {
+            builder.UseMauiCommunityToolkit();
+        }
+#endif
+
+        builder.ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            fonts.AddFont("fa-solid-900.ttf", "FontAwesome");
+        });
 
         builder.Services.AddSingleton<Services.ChoreDatabaseService>();
         builder.Services.AddSingleton<App>();
