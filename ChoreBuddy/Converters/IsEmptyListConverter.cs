38bd60cb413161e041923b414ae66b981dff5b0d
Philip Reese <philipreese@mail.com>
2026-01-12 15:36:51 -0500

    updated version build, add missing file

---
+++ ChoreBuddy/Converters/IsEmptyListConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace ChoreBuddy.Converters;
+
+public class IsEmptyListConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value == null)
+        {
+            return true;
+        }
+
+        if (value is System.Collections.IEnumerable enumerable)
+            return !enumerable.GetEnumerator().MoveNext();
+
+        return false;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException("ConvertBack is not supported.");
+    }
+}
